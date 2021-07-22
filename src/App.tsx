f7f82bd410cda18bf6627d9bed454def379d1c1b
Peskarsky <v.peskarskij@gmail.com>
2021-07-22 19:26:07 +0300

    setup google maps api

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,19 @@
 import React from 'react';
+import { Loader } from '@googlemaps/js-api-loader';
+
+const loader = new Loader({
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  version: 'weekly',
+});
+
+let map: google.maps.Map;
+
+loader.load().then(() => {
+  map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+});
 
 const App: React.FC = () => {
   return (
